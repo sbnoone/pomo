b627b6eb380ae0b40f845ee66bc116f370be1e1b
Slava Baklanov <za4em.new.acc@gmail.com>
2023-05-24 16:03:30 +0300

    update dark theme

--- src/components/input-number.tsx
+++ src/components/input-number.tsx
@@ -7,24 +7,24 @@ interface InputNumberProps extends InputHTMLAttributes<HTMLInputElement> {
 
 export const InputNumber: FC<InputNumberProps> = ({ ...props }) => {
 	return (
-		<div className='flex border-[1px] h-10 border-black-100 rounded-sm overflow-hidden '>
+		<div className='flex border-[1px] h-10 border-black-100 dark:border-white-100 rounded-sm overflow-hidden '>
 			<input
 				{...props}
 				type='number'
-				className='h-10 bg-transparent border-none max-w-[66px] text-center outline-none px-1'
+				className='h-10 bg-transparent border-none max-w-[66px] text-center outline-none px-1 tracking-[0.15px]'
 			/>
-			<div className='border-l-[1px] border-l-black-100'>
+			<div className='border-l-[1px] border-l-black-100 dark:border-l-white-100'>
 				<button
 					type='button'
 					className='flex justify-center items-center w-[30px] h-[19px] -outline-offset-2'
 				>
-					<TriangleSvg />
+					<TriangleSvg className='stroke-blue-900 dark:stroke-blue-50 fill-blue-900  dark:fill-blue-50 [&_path]:fill-blue-900 [&_path]:dark:fill-blue-50' />
 				</button>
 				<button
 					type='button'
-					className='flex justify-center items-center w-[30px] h-[19px] border-t-[1px] border-t-black-100 -outline-offset-2'
+					className='flex justify-center items-center w-[30px] h-[19px] border-t-[1px] border-t-black-100 dark:border-t-white-100 -outline-offset-2'
 				>
-					<TriangleSvg className='rotate-180' />
+					<TriangleSvg className='stroke-blue-900 dark:stroke-blue-50 fill-blue-900  dark:fill-blue-50 [&_path]:fill-blue-900 [&_path]:dark:fill-blue-50 rotate-180' />
 				</button>
 			</div>
 		</div>
--- src/components/switch.tsx
+++ src/components/switch.tsx
@@ -9,16 +9,19 @@ export const Switch = () => {
 			checked={checked}
 			onChange={setChecked}
 			className={clsx('relative inline-flex h-5 w-[34px] items-center rounded-full', {
-				'bg-black-200': !checked,
+				'bg-black-200 dark:bg-white-200': !checked,
 				'bg-blue-600': checked,
 			})}
 		>
-			<span className='sr-only'>Enable notifications</span>
+			<span className='sr-only'>Enable dark mode</span>
 			<span
-				className={clsx('inline-block h-4 w-4 transform rounded-full bg-blue-50 transition', {
-					'translate-x-0.5': !checked,
-					'translate-x-4': checked,
-				})}
+				className={clsx(
+					'inline-block h-4 w-4 transform rounded-full bg-blue-50 dark:bg-blue-950 transition',
+					{
+						'translate-x-0.5': !checked,
+						'translate-x-4': checked,
+					}
+				)}
 			/>
 		</HeadlessSwitch>
 	)
