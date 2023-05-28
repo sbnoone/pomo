bf8f6e3ce7882b9df700553746577f58ac95226c
Slava Baklanov <za4em.new.acc@gmail.com>
2023-05-28 12:10:01 +0300

    update button styles

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -11,13 +11,13 @@ export const Button: FC<ButtonProps> = ({ size, variant, children, ...props }) =
 		<button
 			type='button'
 			className={clsx(
-				'text-blue-900 dark:text-blue-50 dark:[&>svg_path]:stroke-blue-50 dark:[&>svg_path]:fill-blue-50',
+				'text-blue-900 dark:text-blue-50 dark:[&>svg_path]:stroke-blue-50 dark:[&>svg_path]:fill-blue-50 transition-colors',
 				{
 					'rounded-sm p-2': size === 'sm',
 					'rounded-md p-6': size === 'md',
 					'rounded-lg px-12 py-8': size === 'lg',
-					'bg-blue-600': variant === 'primary',
-					'bg-blue-100': variant === 'secondary',
+					'bg-blue-600 hover:bg-blue-700 active:bg-blue-800': variant === 'primary',
+					'bg-blue-100 hover:bg-blue-200 active:bg-blue-300': variant === 'secondary',
 					'bg-transparent': variant === 'transparent',
 				}
 			)}
